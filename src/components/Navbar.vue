d83752b49f4c816d85010846094257cae641d856
Ai Nguyen <ainguyenkaka@gmail.com>
2018-04-19 10:22:57 +0800

    remove navbar on small screen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="m-header">
+  <div class="m-header d-none d-md-block">
     <header class="header navbar fixed-top navbar-expand-md">
       <div class="container">
-          <!-- <a class="navbar-brand text-white" href="#home">
-              <strong>StatNLP Workshop</strong>
-          </a> -->
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headernav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="lnr lnr-text-align-right"></span>
-          </button>
           <div class="collapse navbar-collapse flex-sm-row-reverse" id="headernav">
               <ul class=" nav navbar-nav menu">
                   <li class="nav-item">
