2fbe265fe9ce67ea4af74f966ce2616582081f18
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-19 15:21:25 +0800

    change speaker more image

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@
     Copyright &copy; All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                 <p class="text-center">
-                  Copyright &copy; {{year}} All rights reserved | SUTD StatNLP
+                  Copyright &copy; {{year}} All rights reserved | SUTD <a target="_blank" href="http://statnlp.org/">StatNLP</a>
                 </p>
                 </div>
 
